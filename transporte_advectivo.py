419822acedc1a08312e5e5415d6e371d17bf606a
Lucas Menezes <lucasmenezes4502@gmail.com>
2021-02-15 22:04:14 -0300

    Animacao e Correcoes
    Inclui a parte do código que anima o plot, além de umas correções mínimas.
    NOTA: demora bastante para a animacao chegar no ultimo instante de tempo...mas tem resultado.

--- transporte_advectivo.py
+++ transporte_advectivo.py
@@ -2,6 +2,9 @@
 #importando bibliotecas que hão de ser utilizadas:
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+from matplotlib.animation import FuncAnimation
+
 
 # definindo variaveis
 dt = 1 # passo de tempo (livre)
@@ -15,8 +18,8 @@ dx = 100 # resolucao da grade
 dy = 100
 
 # velocidade de 5 (separado em componentes da raiz de 5 p cada)
-u_const = 5**(1/2)
-v_const = 5**(1/2)
+u_const = (25/2) ** (1 / 2)  # Modulo da velocidade do vento =  5 m/s
+v_const = (25/2) ** (1 / 2)
 
 kx = 1 # coeficiente de difusao (livre)
 
@@ -34,6 +37,7 @@ sup_j = 6
 
 est=(u_const*dt)/dx #tem que ser menor que 1
 beta=(kx*dt)/(dx^2) #tem que ser menor que 0.5
+print(est, beta)
 
 # if ((u_const*dt)/dx == (v_const*dt)/dy): print ('foi') #testando se as constantes sao iguais tanto no x quanto no y
 
@@ -47,35 +51,56 @@ for t in range(nt-1): # botamos nt-1 pra nao dar porblema de indice
 				if i==0 or j==0 or i==i_max or j==j_max: # definindo condicao de contorno = 0 
 					matrix[t][i][j] = 0
 				else: #trabalhando como cond inicial = 25
-					matrix[t][i][j] = 25
+					# matrix[t][i][j] = 25
 					# fazendo o chute inicial
-					difx= (matrix[t, i+1,j] - 2*matrix[t,i,j] + matrix[t, i-1,j])/dx**2
-					dify= (matrix[t, i,j+1] - 2*matrix[t,i,j] + matrix[t, i,j-1])/dy**2
-					advx= (matrix[t, i+1,j] - matrix[t,i-1,j])/dx*2
-					advy= (matrix[t, i,j+1] - matrix[t,i,j-1])/dy*2
+					difx= (matrix[t, i+1,j] - 2*matrix[t,i,j] + matrix[t, i-1,j])/(dx**2)
+					dify= (matrix[t, i,j+1] - 2*matrix[t,i,j] + matrix[t, i,j-1])/(dy**2)
+					advx= (matrix[t, i+1,j] - matrix[t,i-1,j])/(dx*2)
+					advy= (matrix[t, i,j+1] - matrix[t,i,j-1])/(dy*2)
 
-					matrix[t+1,i,j] = (dt*(-kx*(difx+dify)-(u_const*advx)-(v_const*advy)))+matrix[t,i,j]
+					matrix[t+1,i,j] = (dt*(kx*(difx+dify)-(u_const*advx)-(v_const*advy)))+matrix[t,i,j]
 
 	else:		
 		for i in range(1, i_max-1):
 			for j in range(1, j_max-1):
-				if j<=sup_j and j>=inf_j and i<=sup_i and i>=inf_i:
+				if sup_j-1 >= j >= inf_j and sup_i-1 >= i >= inf_i:  # Simplifcado
 					# excluindo a fonte de calor(ja q ela eh cte=25)
 					continue
 				else: #avancando no tempo
-					difx= (matrix[t, i+1,j] - 2*matrix[t,i,j] + matrix[t, i-1,j])/dx**2
-					dify= (matrix[t, i,j+1] - 2*matrix[t,i,j] + matrix[t, i,j-1])/dy**2
-					advx= (matrix[t, i+1,j] - matrix[t,i-1,j])/dx*2
-					advy= (matrix[t, i,j+1] - matrix[t,i,j-1])/dy*2
-					matrix[t+1,i,j] = (2*dt*(-kx*(difx+dify)-(u_const*advx)-(v_const*advy)))+matrix[t-1,i,j]
+					difx= (matrix[t, i+1,j] - 2*matrix[t,i,j] + matrix[t, i-1,j])/(dx**2)
+					dify= (matrix[t, i,j+1] - 2*matrix[t,i,j] + matrix[t, i,j-1])/(dy**2)
+					advx= (matrix[t, i+1,j] - matrix[t,i-1,j])/(dx*2)
+					advy= (matrix[t, i,j+1] - matrix[t,i,j-1])/(dy*2)
+					matrix[t+1,i,j] = (2*dt*(kx*(difx+dify)-(u_const*advx)-(v_const*advy)))+matrix[t-1,i,j]
 
 
-#print(matrix)
+# Eixos
+x = np.arange(0, i_max)
+y = np.arange(0, j_max)
+X, Y = np.meshgrid(x, y, indexing='ij')
 
-x = np.arange(0,i_max)
-y = np.arange(0,j_max)
-teste = matrix[20,:,:]
-X,Y = np.meshgrid(x,y,indexing='ij')
-plt.contourf(X,Y,teste)
-plt.colorbar()
+# Propriedades do plot
+vmin = math.floor(np.min(matrix))
+vmax = math.ceil(np.max(matrix))
+print(vmin, vmax)  # se estes valores tiverem uma diferencia enorme, vai dar problema no plot e nas operacoes seguintes.
+plot_kwargs = {'vmin': vmin, 'vmax': vmax}
+levels = np.arange(vmin, vmax, 1)
+
+# Plotar o Contourf da matrix
+
+fig, ax1 = plt.subplots()
+plot = ax1.contourf(X, Y, matrix[0], levels, **plot_kwargs)
+cb = fig.colorbar(plot)
+
+
+# Animacao do Plot
+def update_plot(frame):
+    ax1.clear()
+    count = ax1.contourf(X, Y, matrix[frame], levels, **plot_kwargs)
+    return count,
+
+
+ani = FuncAnimation(fig, update_plot, np.arange(nt), interval=5)  # se estiver muito rapido, tenta aumentar interval
+
+# Mostra o plot
 plt.show()
